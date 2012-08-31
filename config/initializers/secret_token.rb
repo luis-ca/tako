e0d63f91986c7fa5256ae0b2d47bbc7c4ae854d9
Luis Correa d'Almeida <luis.ca@gmail.com>
2012-08-31 15:16:35 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newgulpd::Application.config.secret_token = 'c7af3b29b21c52a61ca01005869f131316bc05bc89568f0afbdb5ae57f23108734ace33e3ca704ee637af2276eadcc36a6bade47ff1dd1004029da9e99989c21'
